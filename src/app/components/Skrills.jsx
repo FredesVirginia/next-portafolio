8944253c0cdc83f285bcbfb4c12d77d5885adeee
Virginia <WIlliam-Champion@outlook.com>
2023-11-27 21:56:36 -0300

    skils

--- src/app/components/Skrills.jsx
+++ src/app/components/Skrills.jsx
@@ -7,14 +7,29 @@ import { SiMysql } from "react-icons/si";
 import { SiPostgresql } from "react-icons/si";
 import { TbBrandMongodb } from "react-icons/tb";
 import { FaGithub } from "react-icons/fa";
-
+import Image from "next/image"
 export default function Skrills (){
    return (
-    <div className="lg:px-28">
-        <h1>Skrills</h1>
-        <div>
-
-        </div>
+    <div className="px-10 lg:px-28">
+        <h1 className='  text-2xl font-bold lg:text-3xl bg-gradient-to-r from-sky-600 to-sky-900 bg-clip-text text-transparent'>Skrills</h1>
+        <div className=" pl-10 lg:pl-44 grid  grid-cols-3 gap-4 justify-center ">
+     <IoLogoFirebase className="text-5xl" />
+     <FaReact className="text-5xl" />
+     <SiRedux  className="text-5xl" />
+     <FaLaravel className="text-5xl"/>
+     <SiExpress className="text-5xl"/>
+     <SiMysql className="text-5xl"/>
+     <SiPostgresql className="text-5xl"/>
+     <TbBrandMongodb className="text-5xl" />
+     <FaGithub className="text-5xl"/>
+     <Image
+        src="/Img/next.png"
+        width={60}
+        height={60}
+        alt="Picture of the author"
+        className=''
+      />
+    </div>
     </div>
    ) 
 }
